7c0b077b9f8b09a1e1696644cefc8f4c0d0ffdc5
philippepeter <philippe.peter@gmail.com>
2018-01-31 21:58:16 +0100

    init

--- src/main/java/com/oleamedical/Combine.java
+++ src/main/java/com/oleamedical/Combine.java
@@ -3,7 +3,44 @@ package com.oleamedical;
 public final class Combine {
 
     public final static Object[][] combine(Object[][] data) {
-        return null;
+        int[] indexes = new int[data.length];
+        int lines = computeResultLength(data);
+        Object[][] result = new Object[lines][];
+
+        for(int i=0;i<lines;i++) {
+            result[i] = getResult(indexes, data);
+            if(i!=lines-1) {
+                increment(indexes, data.length-1, data);
+            }
+        }
+
+        return result;
+    }
+
+    private static Object[] getResult(int[] indexes, Object[][] data) {
+        Object[] line = new Object[indexes.length];
+        for (int i = 0; i < indexes.length; i++) {
+            line[i] = data[i][indexes[i]];
+        }
+        return line;
+    }
+
+    private static void increment(int[] indexes, int rank, Object[][] data) {
+        if(indexes[rank]<data[rank].length-1) {
+            indexes[rank]++;
+        } else {
+            indexes[rank] = 0;
+            increment(indexes, rank-1,data);
+
+        }
+    }
+
+    private static int computeResultLength(Object[][] data) {
+        int length = 1;
+        for(Object[] array : data) {
+            length *= array.length;
+        }
+        return length;
     }
 
 }
